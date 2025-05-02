8b795899a575a79e74761716c092da67311f9c92
Muhammad Qasim <malikqasim20051@gmail.com>
2025-05-02 15:38:12 +0500

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
+var back_to_top = $('#back_to_top');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+    back_to_top.addClass('show');
+    } else {
+    back_to_top.removeClass('show');
+    }
+  });
+  
+  back_to_top.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+
+
 
 let body = document.querySelector("#body");
 let nav_collapse = document.querySelector("#nav_collapse");
