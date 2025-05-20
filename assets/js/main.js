f1f3935a43bf4247641a3daadf587e7738ee9f18
Muhammad Qasim <malikqasim20051@gmail.com>
2025-05-20 14:17:43 +0500

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,21 @@
 
 
 
-window.addEventListener("load", function () {
-  // Add delay before triggering "loaded"
-  setTimeout(() => {
-    document.body.classList.add("loaded");
+// window.addEventListener("load", function () {
+//   // Add delay before triggering "loaded"
+//   setTimeout(() => {
+//     document.body.classList.add("loaded");
 
-    // Then fade out and remove preloader
-    setTimeout(() => {
-      const preloader = document.getElementById("pre_loader");
-      if (preloader) {
-        preloader.remove();
-      }
-    }, 500); // Fade-out duration
-  }, 2000); // Delay after page is fully loaded (in ms)
-});
+//     // Then fade out and remove preloader
+//     setTimeout(() => {
+//       const preloader = document.getElementById("pre_loader");
+//       if (preloader) {
+//         preloader.remove();
+//       }
+//     }, 500); // Fade-out duration
+//   }, 2000); // Delay after page is fully loaded (in ms)
+// });
+
 
 var back_to_top = $('#back_to_top');
 
