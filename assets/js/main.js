3a77d3f34d57dd67c9b69e39daf6be213bd4e544
Muhammad Qasim <malikqasim20051@gmail.com>
2025-06-01 17:45:10 +0500

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
   AOS.init();
 
 
-// window.addEventListener("load", function () {
-//   // Add delay before triggering "loaded"
-//   setTimeout(() => {
-//     document.body.classList.add("loaded");
+window.addEventListener("load", function () {
+  // Add delay before triggering "loaded"
+  setTimeout(() => {
+    document.body.classList.add("loaded");
 
-//     // Then fade out and remove preloader
-//     setTimeout(() => {
-//       const preloader = document.getElementById("pre_loader");
-//       if (preloader) {
-//         preloader.remove();
-//       }
-//     }, 500); // Fade-out duration
-//   }, 2000); // Delay after page is fully loaded (in ms)
-// });
+    // Then fade out and remove preloader
+    setTimeout(() => {
+      const preloader = document.getElementById("pre_loader");
+      if (preloader) {
+        preloader.remove();
+      }
+    }, 500); // Fade-out duration
+  }, 2000); // Delay after page is fully loaded (in ms)
+});
 
 
 
